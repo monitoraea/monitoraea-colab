0e6bbc870d9f9f8ba5c739c8c10c15d06034e5b0
Ricardo Zylbergeld <ricardo@engajados.com.br>
2024-06-11 16:45:26 +0300

    local build docker

---
+++ local_build_and_run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+export ENV_PATH="./packages/backend/.env.development"
+
+CONTAINER_NAME=dev-monitoraea-colab
+IMAGE_NAME=$CONTAINER_NAME-image:1.0.0
+PORT=4446
+VIRTUAL_HOST=teste.monitoraea.org.br
+VIRTUAL_PORT=$PORT
+LETSENCRYPT_HOST=$VIRTUAL_HOST
+LETSENCRYPT_EMAIL="ricardo@digitao.com.br"
+
+docker rm -f $CONTAINER_NAME
+docker image rm $IMAGE_NAME
+
+docker build \
+    --tag $IMAGE_NAME \
+    --compress \
+    --force-rm \
+    --no-cache \
+    .
+
+docker run \
+    --detach \
+    --restart always \
+    --name $CONTAINER_NAME \
+    --publish $PORT \
+    --env-file $ENV_PATH \
+    --env VIRTUAL_HOST=$VIRTUAL_HOST \
+    --env VIRTUAL_PORT=$VIRTUAL_PORT \
+    --env LETSENCRYPT_HOST=$LETSENCRYPT_HOST \
+    --env LETSENCRYPT_EMAIL=$LETSENCRYPT_EMAIL \
+    --net=host \
+    $IMAGE_NAME
