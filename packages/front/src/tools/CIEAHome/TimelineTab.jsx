253be422966dd1245ab58d1ecfa53703a8b959e2
Ricardo Zylbergeld <ricardo@engajados.com.br>
2024-06-24 07:57:40 +0000

    timeline structure

--- packages/front/src/tools/CIEAHome/TimelineTab.jsx
+++ packages/front/src/tools/CIEAHome/TimelineTab.jsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback, useRef } from 'react';
+import { TextField, Switch, FormGroup, Stack, MenuItem, unstable_useId } from '@mui/material';
 import Helpbox from '../CMS/helpbox';
 import { useQuery, useMutation, useQueryClient } from 'react-query';
 import { useDorothy } from 'dorothy-dna-react';
@@ -11,6 +12,16 @@ import FilePlus from '../../components/icons/FilePlus';
 import Card from '../../components/Card';
 import HelpBoxButton from './HelpBoxButton';
 import GetHelpButton from './GetHelpButton';
+import UploaderField from '../../components/UploaderField';
+
+import Plus from '../../components/icons/plus.svg?react';
+import Trash from '../../components/icons/trash.svg?react';
+
+import { DndProvider, useDrag, useDrop } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+
+import update from 'immutability-helper'
+import { v4 as uuidv4 } from 'uuid';
 
 /* style */
 import style from './timeline.module.scss';
@@ -26,7 +37,29 @@ export default function TimelineTab({ entityId }) {
   const [entity, _entity] = useState([]);
 
   const [contentText, _contentText] = useState(null);
-  
+
+  const [cards, _cards] = useState([
+    {
+      id: 1,
+      date: 'No início de 2011',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet enim lectus. Etiam quis euismod justo. Morbi placerat facilisis pretium. Cras euismod urna in nibh lobortis porttitor. Ut interdum a ipsum at viverra. Cras eget consectetur augue, ut pellentesque nisi. Nam a dapibus nisl. Proin urna ex, tempor in elit vel, laoreet tristique mauris. Mauris scelerisque, urna imperdiet efficitur mattis, nisl lacus rutrum urna, ut pulvinar lacus nisi ut purus. Sed euismod iaculis tristique. Aliquam ullamcorper mollis elit sit amet gravida. Sed placerat tempor eros eu sollicitudin. Aliquam sed metus convallis, ultricies urna vitae, sagittis dui. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+    },
+    {
+      id: 2,
+      date: '2002',
+      content: 'Sed ac interdum purus. Suspendisse tincidunt faucibus dolor eget finibus. Duis ac porta tellus, porttitor dictum lacus. Fusce nec scelerisque felis. Sed auctor orci a iaculis iaculis. In vel quam ipsum. Mauris non viverra lacus',
+    },
+    {
+      id: 3,
+      date: 'Final de 2003',
+      content: 'In congue condimentum lectus, sed faucibus quam fermentum et. Vivamus erat velit, ornare ac viverra vel, vehicula id urna. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec ex nisi, volutpat ut massa elementum, iaculis tincidunt massa. Nullam lacinia, purus sit amet aliquet efficitur, ex felis auctor erat, in iaculis ligula ex id tortor. Duis et tincidunt neque, sed rutrum magna. Sed et mauris vitae nibh blandit pellentesque',
+    },
+    {
+      id: 4,
+      date: '2005',
+      content: 'Phasellus felis mauris, scelerisque id efficitur vitae, tempus vitae justo. Cras at sodales libero, ut sollicitudin lectus. Fusce egestas lectus ac consequat vehicula. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam euismod lobortis lacus ut malesuada. Praesent ac rhoncus arcu. Donec sit amet nibh justo. Proin posuere sem justo, vel tincidunt lectus hendrerit in. Duis venenatis semper fringilla',
+    },
+  ])
 
   //get commission_data
   const { data } = useQuery(['commission_info', { entityId }], {
@@ -139,6 +172,79 @@ export default function TimelineTab({ entityId }) {
       });
     }
   };
+  const changeField = (index, field) => (e) => {
+    _cards((prevCards) =>
+      update(prevCards, {
+        $splice: [
+          [index, 1],
+          [index, 0, {
+            ...prevCards[index],
+            [field]: e.target.value,
+          }],
+        ],
+      }),
+    )
+  }
+  const append = () => {
+    _cards((prevCards) =>
+      update(prevCards, {
+        $push: [
+          {
+            id: uuidv4(),
+            date: '',
+            content: '',
+          }
+        ],
+      }),
+    )
+  }
+  const remove = (index) => () => {
+    _cards((prevCards) =>
+      update(prevCards, {
+        $splice: [
+          [index, 1],
+        ],
+      }),
+    )
+  }
+  const addAbove = (index) => () => {
+    _cards((prevCards) =>
+      update(prevCards, {
+        $splice: [
+          [index, 0, {
+            id: uuidv4(),
+            date: '',
+            content: '',
+          }],
+        ],
+      }),
+    )
+  }
+  const moveCard = useCallback((dragIndex, hoverIndex) => {
+    _cards((prevCards) =>
+      update(prevCards, {
+        $splice: [
+          [dragIndex, 1],
+          [hoverIndex, 0, prevCards[dragIndex]],
+        ],
+      }),
+    )
+  }, [])
+  const renderCard = useCallback((card, index) => {
+    return (
+      <TLCard
+        key={card.id}
+        index={index}
+        id={card.id}
+        date={card.date}
+        content={card.content}
+        moveCard={moveCard}
+        remove={remove}
+        addAbove={addAbove}
+        changeField={changeField}
+      />
+    )
+  }, [])
 
   return (
     <>
@@ -150,7 +256,7 @@ export default function TimelineTab({ entityId }) {
 
                 <section id="details">
                   <div className="section-header">
-                  <div className="section-title"></div>
+                    <div className="section-title"></div>
                     <div className="section-actions">
                       <GetHelpButton tab="timeline" />
 
@@ -161,7 +267,16 @@ export default function TimelineTab({ entityId }) {
                     </div>
                   </div>
 
-                  
+                  <DndProvider backend={HTML5Backend}>
+                    <div>{cards.map((card, i) => renderCard(card, i))}</div>
+                  </DndProvider>
+
+
+                  <div className={`row ${style.add_below}`}>
+                    <button className={style.tl_button} onClick={append}>
+                      <Plus></Plus>
+                    </button>
+                  </div>
 
                 </section>
 
@@ -178,4 +293,115 @@ export default function TimelineTab({ entityId }) {
 
 function TitleAndHelpbox({ title, keyRef, openHelpbox }) {
   return (<div style={{ display: 'flex', alignItems: 'center' }}><div>{title}</div> <HelpBoxButton keyRef={keyRef} openHelpbox={openHelpbox} /></div>)
+}
+
+const TLCard = ({ id, date, content, index, moveCard, remove, addAbove, changeField }) => {
+  const ref = useRef(null)
+  const [{ handlerId }, drop] = useDrop({
+    accept: ItemTypes.TIMELINE_ITEM,
+    collect(monitor) {
+      return {
+        handlerId: monitor.getHandlerId(),
+      }
+    },
+    hover(item, monitor) {
+      if (!ref.current) {
+        return
+      }
+      const dragIndex = item.index
+      const hoverIndex = index
+      // Don't replace items with themselves
+      if (dragIndex === hoverIndex) {
+        return
+      }
+      // Determine rectangle on screen
+      const hoverBoundingRect = ref.current?.getBoundingClientRect()
+      // Get vertical middle
+      const hoverMiddleY =
+        (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2
+      // Determine mouse position
+      const clientOffset = monitor.getClientOffset()
+      // Get pixels to the top
+      const hoverClientY = clientOffset.y - hoverBoundingRect.top
+      // Only perform the move when the mouse has crossed half of the items height
+      // When dragging downwards, only move when the cursor is below 50%
+      // When dragging upwards, only move when the cursor is above 50%
+      // Dragging downwards
+      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+        return
+      }
+      // Dragging upwards
+      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+        return
+      }
+      // Time to actually perform the action
+      moveCard(dragIndex, hoverIndex)
+      // Note: we're mutating the monitor item here!
+      // Generally it's better to avoid mutations,
+      // but it's good here for the sake of performance
+      // to avoid expensive index searches.
+      item.index = hoverIndex
+    },
+  })
+  const [{ isDragging }, drag] = useDrag({
+    type: ItemTypes.TIMELINE_ITEM,
+    item: () => {
+      return { id, index }
+    },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  })
+  const opacity = isDragging ? 0 : 1
+  drag(drop(ref))
+  return (
+    <div ref={ref} className={style.tlcard} style={{ opacity }} data-handler-id={handlerId}>
+      <div className={`row ${style.add_above}`}>
+        <button className={style.tl_button} onClick={addAbove(index)}>
+          <Plus></Plus>
+        </button>
+      </div>
+      <div className="row">
+        <div className="col-xs-2">
+          <div className={style.logo_wrapper}>
+            <UploaderField onChange={value => console.log} url={''} alt="imagem" title="Imagem" />
+          </div>
+        </div>
+
+        <div className="col-xs-9">
+          <div className="row">
+            <div className="col-xs-12">
+              <TextField
+                className="input-text"
+                label="Quando"
+                value={date}
+                onChange={changeField(index, 'date')}
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-xs-12">
+              <TextField
+                className="input-text"
+                label="Texto"
+                value={content}
+                onChange={changeField(index, 'content')}
+                multiline
+                rows={3}
+              />
+            </div>
+          </div>
+        </div>
+        <div className={`col-xs-1 ${style.remove}`}>
+          <button className={style.tl_button} onClick={remove(index)}>
+            <Trash></Trash>
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const ItemTypes = {
+  TIMELINE_ITEM: 'timeline-item',
 }
