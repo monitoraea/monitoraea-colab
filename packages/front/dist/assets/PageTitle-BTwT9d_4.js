ff1866290fe20ab1e51939c6133c0a4ee392e6a0
Ricardo Zylbergeld <ricardo@digitao.com.br>
2024-10-10 10:57:53 +0300

    deployment

---
+++ packages/front/dist/assets/PageTitle-BTwT9d_4.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-D3lmPgLZ.js";const s={"page-title":"_page-title_hqntg_45"},i=({title:t})=>e.jsx("div",{className:s["page-title"],children:t});export{i as P};
--- packages/front/dist/assets/useThemeProps-DYjcmG9w.js
+++
@@ -1,1 +0,0 @@
-import{aB as s,aC as r,aD as m}from"./index-B3sBGDZf.js";function t({props:e,name:a}){return s({props:e,name:a,defaultTheme:r,themeId:m})}export{t as u};
