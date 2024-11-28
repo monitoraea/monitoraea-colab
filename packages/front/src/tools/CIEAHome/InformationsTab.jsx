152f494e2a3811cad056dd9b451a9cf4cbf7cf4c
Ricardo Zylbergeld <ricardo@digitao.com.br>
2024-11-28 14:58:32 +0200

    form renderer responsible for data manipulation

--- packages/front/src/tools/CIEAHome/InformationsTab.jsx
+++ packages/front/src/tools/CIEAHome/InformationsTab.jsx
@@ -14,7 +14,7 @@ import Card from '../../components/Card';
 
 import FilePlus from '../../components/icons/FilePlus';
 
-import { Renderer, mapData2Form } from '../../components/FormRenderer'
+import { Renderer, mapData2Form, getFormData } from '../../components/FormRenderer/new'
 
 import form from './form1.yml'
 import form_view from './form1_view.yml'
@@ -23,16 +23,6 @@ import lists from './lists1.yml'
 /* style */
 // import style from './information.module.scss';
 
-const emptyFiles = {
-  'logo_arquivo': null,
-  'documento_criacao_arquivo2': null,
-  'regimento_interno_arquivo2': null,
-  'ppea_arquivo2': null,
-  'ppea2_arquivo2': null,
-  'programa_estadual_arquivo2': null,
-  'plano_estadual_arquivo2': null,
-}
-
 export default function InformationsTab({ entityId }) {
   /* hooks */
   const { server } = useDorothy();
@@ -40,9 +30,9 @@ export default function InformationsTab({ entityId }) {
   const { enqueueSnackbar, closeSnackbar } = useSnackbar();
 
   /* states */
-  const [originalEntity, _originalEntity] = useState({});
+  // const [originalEntity, _originalEntity] = useState({});
   const [entity, _entity] = useState({});
-  const [files, _files] = useState(emptyFiles);
+  const [files, _files] = useState({});
 
   //get commission_data
   const { data } = useQuery(['commission_info', { entityId }], {
@@ -54,111 +44,21 @@ export default function InformationsTab({ entityId }) {
   // TODO: ABAIXO: responsabilidade do formRenderer?
   // onde digo "responsabilidade do renderer" pode ser responsabilidade de um outro componente controller, fazendo o papel que hoje está com o componente que usa o renderer
 
-  useEffect(() => {
-    if (!data || !form) return;
+  // useEffect(() => {
+  //   if (!data || !form) return;
+  //   _originalEntity(mapData2Form(data, form));
 
-    const mData = mapData2Form(data, form);
+  // }, [data, form]);
 
-    _entity(mData);
-    _originalEntity(mData);
-
-    _files(emptyFiles);
-
-    // console.log(data)
-  }, [data, form]);
-
-  const handleDataChange = (field, value, iterative /* k = block key, index */) => {
-    if (iterative === undefined) { /* campos fora de blocos ou em blocos não iterativos */
-
-      let newEntity = {...entity};
-
-      if(field.includes('tipo') && value === 'link') {
-        console.log()
-
-        const mainField = field.replace('_tipo','');
-        newEntity = {
-          ...newEntity,
-          [`${mainField}_arquivo`]: originalEntity[`${mainField}_tipo`] === 'link' ? originalEntity[`${mainField}_arquivo`] : '',
-        };
-      }
-
-      // handle files
-      if (['logo_arquivo', 'documento_criacao_arquivo2', 'regimento_interno_arquivo2', 'ppea_arquivo2', 'ppea2_arquivo2', 'programa_estadual_arquivo2', 'plano_estadual_arquivo2'].includes(field)) {
-
-        newEntity = {
-          ...newEntity,
-          [field]: value ? value : 'remove',
-        };
-
-        _files({ ...files, [field]: value?.file });
-
-      } else {
-
-        newEntity = {
-          ...newEntity,
-          [field]: value
-        };
-
-      }
-
-      console.log(`changing ${field}:`, value, newEntity);
-
-      _entity(newEntity)
-
-    } else { /* campos em blocos iterativos */
-
-      /* TODO: files */
-
-      let complexValue = entity[iterative.k];
-      if (!!complexValue && Array.isArray(complexValue)) {
-        complexValue[iterative.index][field] = value;
-
-        _entity(entity => ({
-          ...entity,
-          [iterative.k]: complexValue
-        }))
-
-      }
-    }
-  }
-
-  const handleRemoveIterative = (iterative) => {
-    let complexValue = entity[iterative.k];
-    complexValue.splice(iterative.index, 1);
-
-    _entity(entity => ({
-      ...entity,
-      [iterative.k]: complexValue
-    }))
-  }
-
-  const handleAddIterative = (block) => {
-
-    let newEmptyValue = {};
-    for (let field of block.elements) newEmptyValue[field] = null;
-
-    let newComplexValue;
-    if (!entity[block.key]) newComplexValue = [newEmptyValue];
-    else newComplexValue = [...entity[block.key], newEmptyValue];
-
-    _entity(entity => ({
-      ...entity,
-      [block.key]: newComplexValue
-    }))
+  const handleDataChange = (entity, files) => {
+    _entity(entity)
+    _files(files)
   }
 
   const handleSave = async () => {
 
     /* save */
-    let data = new FormData();
-
-    if (!!files['logo_arquivo']) data.append('logo', files['logo_arquivo']);
-    if (entity.documento_criacao_tipo === 'file' && !!files['documento_criacao_arquivo2']) data.append('documento_criacao', files['documento_criacao_arquivo2']);
-    if (entity.regimento_interno_tipo === 'file' && !!files['regimento_interno_arquivo2']) data.append('regimento_interno', files['regimento_interno_arquivo2']);
-    if (entity.ppea_tipo === 'file' && !!files['ppea_arquivo2']) data.append('ppea', files['ppea_arquivo2']);
-    if (entity.ppea2_tipo === 'file' && !!files['ppea2_arquivo2']) data.append('ppea2', files['ppea2_arquivo2']);
-    if (entity.programa_estadual_tipo === 'file' && !!files['programa_estadual_arquivo2']) data.append('programa_estadual', files['programa_estadual_arquivo2']);
-    if (entity.plano_estadual_tipo === 'file' && !!files['plano_estadual_arquivo2']) data.append('plano_estadual', files['plano_estadual_arquivo2']);
+    const data = getFormData(form, entity, files) // prepare information (Renderer)
 
     const snackKey = enqueueSnackbar('Gravando...', {
       /* variant: 'info', */
@@ -175,9 +75,8 @@ export default function InformationsTab({ entityId }) {
       /* edit */
       method = 'put';
       url = `${server}commission/${entityId}/draft`;
-      data.set('entity', JSON.stringify(entity));
 
-      const { data: response } = await axios({
+      /* const { data: response } =  */await axios({
         method,
         url,
         data,
@@ -236,10 +135,8 @@ export default function InformationsTab({ entityId }) {
                   form={form}
                   view={form_view}
                   lists={lists}
-                  data={entity}
+                  data={mapData2Form(data, form)}
                   onDataChange={handleDataChange}
-                  onRemoveIterative={handleRemoveIterative}
-                  onAddIterative={handleAddIterative}
                 />
 
                 <div className="section-header">
