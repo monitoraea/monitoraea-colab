9d823b94664ec896c81f7499ef3c917c258d6d0f
Ricardo Zylbergeld <ricardo@digitao.com.br>
2024-12-08 12:23:01 +0200

    form check - verify visibility

--- packages/backend/form_utils/index.js
+++ packages/backend/form_utils/index.js
@@ -1,18 +1,27 @@
 module.exports.check = (form, data) => {
-    // TODO: is visible (own show property or its block property) - function 
-    // TODO: nesting visibility (blocks inside blocks where parent has visibility rule) -> ISSUE
-    
+
+    // merge elements of blocks
+    for (let b of form.blocks) {
+        mergeBlockElement(b, form)
+    }
+
     let is_form_valid = true
     let fields = {}
-    for(let f of form.fields) {
-        if(f.validation && f.validation.length) {
-            if(!Array.isArray(f.validation)) f.validation = [f.validation]
+    for (let f of form.fields) {
 
-            for(let v of f.validation) {
+        if (!isFieldVisible(f, form, data)) { // se não está visivel, então está ok
+            fields[f.key] = true
+            continue
+        }
+
+        if (f.validation && f.validation.length) {
+            if (!Array.isArray(f.validation)) f.validation = [f.validation]
+
+            for (let v of f.validation) {
                 const type = v.type || v
 
                 let error = false
-                switch(type) {
+                switch (type) {
                     case 'neq':
                         error = is_neq(data[f.key], v)
                         break
@@ -20,11 +29,11 @@ module.exports.check = (form, data) => {
                         error = is_empty(data[f.key], f.type, v)
                         break
                     default:
-                        // do nothing
+                    // do nothing
                 }
 
                 fields[f.key] = !error
-                if(error) is_form_valid = false
+                if (error) is_form_valid = false
             }
         }
     }
@@ -36,12 +45,50 @@ module.exports.check = (form, data) => {
 }
 
 function is_neq(data, v) {
-    if(v.value && data === v.value) return true
+    if (v.value && data === v.value) return true
     return false
 }
 
 function is_empty(data, field_type, v) {
-    if(['text', 'textarea'].includes(field_type) && !data.trim().length) return true
-    if(['file', 'thumbnail'].includes(field_type) && !data) return true
+    if (['text', 'textarea'].includes(field_type) && !data.trim().length) return true
+    if (['file', 'thumbnail'].includes(field_type) && !data) return true
     return false
+}
+
+function mergeBlockElement(b, form) {
+    // insere os campos com propriedade block em elements
+    let elements = b.elements ? [...b.elements] : []
+    for (let f of form.fields.filter(f => f.block === b.key)) {
+        elements.push(f.key)
+    }
+    elements.sort((a, b) => a.type === 'block' ? 1 : 0)
+
+    b.elements = [...b.elements || [], ...elements]
+}
+
+function isFieldVisible(f, form, data) {
+    // TODO: nesting visibility (blocks inside blocks where parent has visibility rule) -> ISSUE
+
+    // FIELDS
+    let show = checkShow(f, data)
+    // PARENT BLOCK
+    if (show) {
+        // qual o bloco do campo?
+        const block = form.blocks.find(b => b.elements.includes(f.key))
+        // analisa visibilidade do bloco
+        if (block) show = checkShow(block, data)
+    }
+
+    return show
+}
+
+function checkShow(e, data) {
+    let show = true
+
+    if (e.show?.target) {
+        if (!Array.isArray(e.show.target.value)) show = (data[e.show.target.key] === e.show.target.value);
+        else show = e.show.target.value.includes(data[e.show.target.key])
+    }
+
+    return show
 }
