d602b0f2cc11570a260723a42a7f864c46327627
Ricardo Zylbergeld <ricardo@digitao.com.br>
2025-05-25 18:17:20 +0300

    CMS - fix bug that clears th title field

--- packages/front/src/tools/CMS/manager.jsx
+++ packages/front/src/tools/CMS/manager.jsx
@@ -214,7 +214,8 @@ export default function CMS({ id, onClose, onSave }) {
       hd = !!entity.portal ? secureFindIndex(dynamicContents[entity.portal], helpbox.keyref, 1) : '';
 
     _helpbox_description(hd);
-    _entity(entity => ({ ...entity, title: hd }));
+    if(hd.length) _entity(entity => ({ ...entity, title: hd }));
+
   }, [entity.helpbox, entity.portal]);
 
   useEffect(() => {
