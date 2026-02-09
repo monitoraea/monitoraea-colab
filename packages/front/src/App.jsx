55be4c815d7cf32ef39001cf1719ecb3bbbb2ab0
Ricardo Zylbergeld <ricardo@digitao.com.br>
2026-02-09 18:07:36 +0200

    logout if token is invalid

--- packages/front/src/App.jsx
+++ packages/front/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Dorothy } from 'dorothy-dna-react';
 
@@ -25,6 +25,7 @@ function Prep() {
   return (<strong>...</strong>)
 }
 function App() {
+  const [me_verified, _me_verified] = useState(false);
 
   const queryClient = new QueryClient({
     defaultOptions: {
@@ -34,6 +35,31 @@ function App() {
       },
     },
   })
+
+  useEffect(() => {
+    async function verifyMe() {
+      try {
+        await axios.get(`${config.server}user/me`,
+          {
+            headers: {
+              'X-Dorothy-Token': localStorage.getItem(config.app_name + "-dorothy-token"),
+            },
+          });
+
+      } catch (e) {
+        console.log('logout');
+        localStorage.removeItem(config.app_name + "-dorothy-token");
+        delete axios.defaults.headers.common['X-Dorothy-Token'];
+      } finally {
+        _me_verified(true);
+      }
+
+    }
+
+    verifyMe();
+  }, [])
+
+  if (!me_verified) return <>... ...</>;
 
   return (<div id="App">
     <Dorothy config={config} preparingEl={<Prep />}>
